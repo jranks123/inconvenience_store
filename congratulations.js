8c02c6d2d9eff0bf2c2a310bea5e8b57ed1c565e
jranks123 <jonathan.rankin@mytutor.co.uk>
2024-08-04 03:24:50 +0100

    message

---
+++ congratulations.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const firstName = urlParams.get('firstName');
+    const lastName = urlParams.get('lastName');
+
+    const attempts = localStorage.getItem('attempts');
+    console.log(attempts);
+    const totalTime = Math.floor((Date.now() - parseInt(localStorage.getItem('startTime'))) / 1000);
+
+    const congratulationsMessage = document.getElementById('congratsMessage');
+    congratulationsMessage.textContent = `Congratulations ${firstName} ${lastName}, you have completed the online steps of sign up in ${attempts} attempts over ${totalTime} seconds. Please show this screen to a member of staff to get the next form.`;
+  
+});
